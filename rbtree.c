4330a30e146f90c6cfd63d467b533840c533eed5
Hyeon Kim <simnalamburt@gmail.com>
2015-05-26 04:48:47 +0900

    Adopt wikipedia code

--- rbtree.c
+++ rbtree.c
@@ -8,7 +8,7 @@
 // Node of red-black tree
 //
 typedef uint32_t data_t;
-typedef enum { red, black } color_t;
+typedef enum { RED, BLACK } color_t;
 typedef struct node {
   data_t data;
   color_t color;
@@ -64,6 +64,25 @@ int main() {
 //
 // Insertion
 //
+node_t *grandparent(node_t *n) {
+  return n && n->parent ? n->parent->parent : NULL;
+}
+
+node_t *uncle(node_t *n) {
+  node_t *g = grandparent(n);
+  if (!g) { return NULL; }
+
+  return n->parent == g->left ? g->right : g->left;
+}
+
+static void insert_case1(node_t*);
+static void insert_case2(node_t*);
+static void insert_case3(node_t*);
+static void insert_case4(node_t*);
+static void insert_case5(node_t*);
+static void rotate_left(node_t*);
+static void rotate_right(node_t*);
+
 void insert(node_t **root, data_t data) {
   // Allocate memory for a new node
   node_t *z = malloc(sizeof(node_t));
@@ -72,11 +91,10 @@ void insert(node_t **root, data_t data) {
 
   // If root is NULL, set z as root and return
   if (*root == NULL) {
-    z->color = black;
+    z->color = BLACK;
     (*root) = z;
     return;
   }
-
 
   //
   // Standard BST insertion
@@ -86,91 +104,73 @@ void insert(node_t **root, data_t data) {
     y = x;
     x = (z->data < x->data) ? x->left : x->right;
   }
-  z->color = red;
+  z->color = RED;
   z->parent = y;
-  if (z->data > y->data) {
-    y->right = z;
-  } else {
+  if (z->data < y->data) {
     y->left = z;
+  } else {
+    y->right = z;
   }
 
-
-  //
-  // Fixup the Red-Black tree
-  //
-
-  // Iterate until z is not a root, and z's parent color is red
-  while (z != *root && z->parent->color == red) {
-    // Find uncle
-    node_t *uncle = z->parent == z->parent->parent->left ?
-      z->parent->parent->right :
-      z->parent->parent->left;
-
-    // Check if the uncle is RED
-    if (uncle->color == red) {
-      z->parent->color = uncle->color = black;
-      z->parent->parent->color = red;
-      z = z->parent->parent;
-      continue;
-    }
-
-    // Otherwise, there are four cases; LL, LR, RL and RR
-    void rotate_left(node_t*);
-    void rotate_right(node_t*);
-
-    if (z->parent == z->parent->parent->left && z == z->parent->left) {
-      // On Left-Left (LL) case
-      //
-      // 1. Swap color of parent and grandparent
-      // 2. Right Rotate Grandparent
-      color_t c = z->parent->color ;
-      z->parent->color = z->parent->parent->color;
-      z->parent->parent->color = c;
-      rotate_right(z->parent->parent);
-    }
-
-    if (z->parent == z->parent->parent->left && z == z->parent->right) {
-      // On Left-Right (LR) case
-      //
-      // 1. Swap color of current node  and grandparent
-      // 2. Left Rotate Parent
-      // 3. Right Rotate Grand Parent
-      color_t c = z->color;
-      z->color = z->parent->parent->color;
-      z->parent->parent->color = c;
-      rotate_left(z->parent);
-      rotate_right(z->parent->parent);
-    }
-
-    if (z->parent == z->parent->parent->right && z == z->parent->right) {
-      // On Right-Right (RR) case
-      //
-      // 1. Swap color of parent and grandparent
-      // 2. Left Rotate Grandparent
-      color_t c = z->parent->color;
-      z->parent->color = z->parent->parent->color;
-      z->parent->parent->color = c;
-      rotate_left(z->parent->parent);
-    }
-
-    if (z->parent == z->parent->parent->right && z == z->parent->left) {
-      // On Right-Left (RL) case
-      //
-      // 1. Swap color of current node  and grandparent
-      // 2. Right Rotate Parent
-      // 3. Left Rotate Grand Parent
-      color_t c = z->color;
-      z->color = z->parent->parent->color;
-      z->parent->parent->color = c;
-      rotate_right(z->parent);
-      rotate_left(z->parent->parent);
-    }
-  }
-
-  // Root should always be black
-  (*root)->color = black;
+  insert_case1(z);
 }
 
+void insert_case1(struct node *n)
+{
+  if (n->parent == NULL)
+    n->color = BLACK;
+  else
+    insert_case2(n);
+}
+
+void insert_case2(struct node *n)
+{
+  if (n->parent->color == BLACK)
+    return; /* Tree is still valid */
+  else
+    insert_case3(n);
+}
+
+void insert_case3(struct node *n)
+{
+  struct node *u = uncle(n), *g;
+
+  if ((u != NULL) && (u->color == RED)) {
+    n->parent->color = BLACK;
+    u->color = BLACK;
+    g = grandparent(n);
+    g->color = RED;
+    insert_case1(g);
+  } else {
+    insert_case4(n);
+  }
+}
+
+void insert_case4(struct node *n)
+{
+  struct node *g = grandparent(n);
+
+  if ((n == n->parent->right) && (n->parent == g->left)) {
+    rotate_left(n->parent);
+    n = n->left;
+  } else if ((n == n->parent->left) && (n->parent == g->right)) {
+    rotate_right(n->parent);
+    n = n->right;
+  }
+  insert_case5(n);
+}
+
+void insert_case5(struct node *n)
+{
+  struct node *g = grandparent(n);
+
+  n->parent->color = BLACK;
+  g->color = RED;
+  if (n == n->parent->left)
+    rotate_right(g);
+  else
+    rotate_left(g);
+}
 
 //
 // Helper functions
