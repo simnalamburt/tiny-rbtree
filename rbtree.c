a698d91252767f7b44318cd1992f51751fd9e65f
Hyeon Kim <simnalamburt@gmail.com>
2015-05-26 00:43:30 +0900

    Clean up

--- rbtree.c
+++ rbtree.c
@@ -1,218 +1,228 @@
-// C program for Red-Black Tree insertion
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-//A Red-Black tree node structure
-struct node
-{
-  int data;     // for data part
-  char color;  // for color property
 
-  //links for left, right children and parent
+//
+// Node of red-black tree
+//
+typedef struct node {
+  int data;
+  char color;
   struct node *left, *right, *parent;
-};
+} node;
+
+static void insert(node **root, int data);
+static void traverse_inorder(node *root, void (*)(int data));
 
 
-// Left Rotation
-void LeftRotate(struct node **root,struct node *x)
-{
-  //y stored pointer of right child of x
-  struct node *y = x->right;
+//
+// Test if rbtree works fine
+//
+void per_data(int data) { printf("%d ", data); }
 
-  //store y's left subtree's pointer as x's right child
-  x->right = y->left;
+int main() {
+  node *root = NULL;
+  insert(&root, 5);
+  insert(&root, 3);
+  insert(&root, 7);
+  insert(&root, 2);
+  insert(&root, 4);
+  insert(&root, 6);
+  insert(&root, 8);
+  insert(&root, 11);
 
-  //update parent pointer of x's right
-  if (x->right != NULL)
-    x->right->parent = x;
+  // Expected output: 2 3 4 5 6 7 8 11
+  printf("inorder Traversal Is : ");
+  traverse_inorder(root, per_data);
+  puts("");
 
-  //update y's parent pointer
-  y->parent = x->parent;
-
-  // if x's parent is null make y as root of tree
-  if (x->parent == NULL)
-    (*root) = y;
-
-  // store y at the place of x
-  else if (x == x->parent->left)
-    x->parent->left = y;
-  else    x->parent->right = y;
-
-  // make x as left child of y
-  y->left = x;
-
-  //update parent pointer of x
-  x->parent = y;
+  return 0;
 }
 
 
-// Right Rotation (Similar to LeftRotate)
-void rightRotate(struct node **root,struct node *y)
-{
-  struct node *x = y->left;
-  y->left = x->right;
-  if (x->right != NULL)
-    x->right->parent = y;
-  x->parent =y->parent;
-  if (x->parent == NULL)
-    (*root) = x;
-  else if (y == y->parent->left)
-    y->parent->left = x;
-  else y->parent->right = x;
-  x->right = y;
-  y->parent = x;
-}
 
-// Utility function to fixup the Red-Black tree after standard BST insertion
-void insertFixUp(struct node **root,struct node *z)
-{
-  // iterate until z is not the root and z's parent color is red
-  while (z != *root && z->parent->color == 'R')
-  {
-    struct node *y;
+static void rotate_left(node**, node*);
+static void rotate_right(node**, node*);
 
+
+//
+// Insertion
+//
+void insert(node **root, int data) {
+  // Allocate memory for a new node
+  node *z = malloc(sizeof(node));
+  z->data = data;
+  z->left = z->right = z->parent = NULL;
+
+  // If root is NULL, set z as root and return
+  if (*root == NULL) {
+    z->color = 'B';
+    (*root) = z;
+    return;
+  }
+
+
+  //
+  // Standard BST insertion steps
+  //
+  node *y, *x = (*root);
+  while (x != NULL) {
+    y = x;
+    x = (z->data < x->data) ? x->left : x->right;
+  }
+  z->parent = y;
+  if (z->data > y->data) {
+    y->right = z;
+  } else {
+    y->left = z;
+  }
+  z->color = 'R';
+
+
+  //
+  // Fixup the Red-Black tree
+  //
+
+  // Iterate until z is not a root, and z's parent color is red
+  while (z != *root && z->parent->color == 'R') {
     // Find uncle and store uncle in y
-    if (z->parent == z->parent->parent->left)
-      y = z->parent->parent->right;
-    else
-      y = z->parent->parent->left;
+    node *y = z->parent == z->parent->parent->left ?
+      z->parent->parent->right :
+      z->parent->parent->left;
 
-    // If uncle is RED, do following
-    // (i)  Change color of parent and uncle as BLACK
-    // (ii) Change color of grandparent as RED
-    // (iii) Move z to grandparent
-    if (y->color == 'R')
-    {
+    if (y->color == 'R') {
+      // If uncle is RED
+      //
+      // 1. Change color of parent and uncle as BLACK
+      // 2. Change color of grandparent as RED
+      // 3. Move z to grandparent
       y->color = 'B';
       z->parent->color = 'B';
       z->parent->parent->color = 'R';
       z = z->parent->parent;
-    }
+    } else {
+      // Otherwise, there are four cases; LL, LR, RL and RR
 
-    // Uncle is BLACK, there are four cases (LL, LR, RL and RR)
-    else
-    {
-      // Left-Left (LL) case, do following
-      // (i)  Swap color of parent and grandparent
-      // (ii) Right Rotate Grandparent
-      if (z->parent == z->parent->parent->left &&
-          z == z->parent->left)
-      {
+      if (z->parent == z->parent->parent->left && z == z->parent->left) {
+        // On Left-Left (LL) case
+        //
+        // 1. Swap color of parent and grandparent
+        // 2. Right Rotate Grandparent
         char ch = z->parent->color ;
         z->parent->color = z->parent->parent->color;
         z->parent->parent->color = ch;
-        rightRotate(root,z->parent->parent);
+        rotate_right(root,z->parent->parent);
       }
 
-      // Left-Right (LR) case, do following
-      // (i)  Swap color of current node  and grandparent
-      // (ii) Left Rotate Parent
-      // (iii) Right Rotate Grand Parent
-      if (z->parent == z->parent->parent->left &&
-          z == z->parent->right)
-      {
+      if (z->parent == z->parent->parent->left && z == z->parent->right) {
+        // On Left-Right (LR) case
+        //
+        // 1. Swap color of current node  and grandparent
+        // 2. Left Rotate Parent
+        // 3. Right Rotate Grand Parent
         char ch = z->color ;
         z->color = z->parent->parent->color;
         z->parent->parent->color = ch;
-        LeftRotate(root,z->parent);
-        rightRotate(root,z->parent->parent);
+        rotate_left(root,z->parent);
+        rotate_right(root,z->parent->parent);
       }
 
-      // Right-Right (RR) case, do following
-      // (i)  Swap color of parent and grandparent
-      // (ii) Left Rotate Grandparent
-      if (z->parent == z->parent->parent->right &&
-          z == z->parent->right)
-      {
+      if (z->parent == z->parent->parent->right && z == z->parent->right) {
+        // On Right-Right (RR) case
+        //
+        // 1. Swap color of parent and grandparent
+        // 2. Left Rotate Grandparent
         char ch = z->parent->color ;
         z->parent->color = z->parent->parent->color;
         z->parent->parent->color = ch;
-        LeftRotate(root,z->parent->parent);
+        rotate_left(root,z->parent->parent);
       }
 
-      // Right-Left (RL) case, do following
-      // (i)  Swap color of current node  and grandparent
-      // (ii) Right Rotate Parent
-      // (iii) Left Rotate Grand Parent
-      if (z->parent == z->parent->parent->right &&
-          z == z->parent->left)
-      {
+      if (z->parent == z->parent->parent->right && z == z->parent->left) {
+        // On Right-Left (RL) case
+        //
+        // 1. Swap color of current node  and grandparent
+        // 2. Right Rotate Parent
+        // 3. Left Rotate Grand Parent
         char ch = z->color ;
         z->color = z->parent->parent->color;
         z->parent->parent->color = ch;
-        rightRotate(root,z->parent);
-        LeftRotate(root,z->parent->parent);
+        rotate_right(root,z->parent);
+        rotate_left(root,z->parent->parent);
       }
     }
   }
-  (*root)->color = 'B'; //keep root always black
+
+  // Root should always be black
+  (*root)->color = 'B';
 }
 
-// Utility function to insert newly node in RedBlack tree
-void insert(struct node **root, int data)
-{
-  // Allocate memory for new node
-  struct node *z = (struct node*)malloc(sizeof(struct node));
-  z->data = data;
-  z->left = z->right = z->parent = NULL;
 
-  //if root is null make z as root
-  if (*root == NULL)
-  {
-    z->color = 'B';
-    (*root) = z;
+//
+// Left rotation
+//
+void rotate_left(node **root, node *x) {
+  // Variable 'y' stores the address x's right child
+  node *y = x->right;
+
+  // Store y's left subtree's pointer as x's right child
+  x->right = y->left;
+
+  // Update parent pointer of x's right
+  if (x->right != NULL) {
+    x->right->parent = x;
   }
-  else
-  {
-    struct node *y = NULL;
-    struct node *x = (*root);
 
-    // Follow standard BST insert steps to first insert the node
-    while (x != NULL)
-    {
-      y = x;
-      if (z->data < x->data)
-        x = x->left;
-      else
-        x = x->right;
-    }
-    z->parent = y;
-    if (z->data > y->data)
-      y->right = z;
-    else
-      y->left = z;
-    z->color = 'R';
+  // Update y's parent pointer
+  y->parent = x->parent;
 
-    // call insertFixUp to fix reb-black tree's property if it
-    // is voilated due to insertion.
-    insertFixUp(root,z);
+  // If x's parent is null, make y as root of tree
+  // Otherwise, store y at the place of x
+  if (x->parent == NULL) {
+    *root = y;
+  } else if (x == x->parent->left) {
+    x->parent->left = y;
+  } else {
+    x->parent->right = y;
   }
+
+  // Make x as left child of y
+  y->left = x;
+
+  // Update parent pointer of x
+  x->parent = y;
 }
 
-// A utility function to traverse Red-Black tree in inorder fashion
-void inorder(struct node *root)
-{
-  if (root == NULL)
-    return;
-  inorder(root->left);
-  printf("%d ", root->data);
-  inorder(root->right);
+
+//
+// Right rotation (mirror of rotate_left)
+//
+void rotate_right(node **root, node *y) {
+  node *x = y->left;
+  y->left = x->right;
+  if (x->right != NULL) {
+    x->right->parent = y;
+  }
+  x->parent =y->parent;
+  if (x->parent == NULL) {
+    (*root) = x;
+  } else if (y == y->parent->left) {
+    y->parent->left = x;
+  } else {
+    y->parent->right = x;
+  }
+  x->right = y;
+  y->parent = x;
 }
 
-/* Drier program to test above function*/
-int main()
-{
-  struct node *root = NULL;
-  insert(&root,5);
-  insert(&root,3);
-  insert(&root,7);
-  insert(&root,2);
-  insert(&root,4);
-  insert(&root,6);
-  insert(&root,8);
-  insert(&root,11);
-  printf("inorder Traversal Is : ");
-  inorder(root);
 
-  return 0;
+//
+// Traverse arbitrary binary tree in inorder fashion
+//
+void traverse_inorder(node *node, void (*func)(int data)) {
+  if (!node) { return; }
+
+  traverse_inorder(node->left, func);
+  func(node->data);
+  traverse_inorder(node->right, func);
 }
